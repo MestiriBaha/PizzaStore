b8e9f27f546db11f84f8143f3c62e9f729441f81
baha mestiri <mohamedbaha.mestiri@esprit.tn>
2022-12-03 01:30:15 +0100

    Small Api using Minimal API .NET 6 Feature

--- Program.cs
+++ Program.cs
@@ -3,29 +3,33 @@ using PizzaStore ;
 
 var builder = WebApplication.CreateBuilder(args);
 //adding CORS Service : 
-builder.Services.AddCors(option => { });
+//builder.Services.AddCors(option => { "hello world " });
 //adding swagger service  : 
 builder.Services.AddEndpointsApiExplorer(); 
 builder.Services.AddSwaggerGen(c=>
 {
-    c.SwaggerDoc("Version 1 ", new OpenApiInfo
-    {
-        Title = "Minimal Api ",
-        Description = "following the Microsoft learn path ",
-        Version = "v1",
-    });
+    //30 min in a meaningless bug 
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
+   
 });
+
+
 // Look carefully , swagger service and others are added before the BUILD is done !! 
 var app = builder.Build();
+app.UseDeveloperExceptionPage();
+
 
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
+    
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minimal API V1");
+    c.RoutePrefix = string.Empty;
+
 
 });
 
-app.MapGet("pizzas/{id}", (int id) => PizzaDb.GetPizza(id));
+app.MapGet("/pizzas/{id}", (int id) => PizzaDb.GetPizza(id));
 app.MapGet("/pizzas", () =>PizzaDb.GetPizzas());
 app.MapPost("/pizzas", (Pizza pizza) => PizzaDb.CreatePizza(pizza));
 app.MapPut("/pizzas", (Pizza pizza) => PizzaDb.UpdatePizza(pizza));
